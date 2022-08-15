e42c96ae6ba81db66c2fb0ad50876f3a20b4d760
Mylo <boming.ying@heliang.cc>
2022-08-15 14:27:27 +0800

    feat: add styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,28 @@
 import Head from "next/head"
 import { isObject, deepCopy } from "../utils"
+import React from "react"
 
+import styled from 'styled-components';
+
+// Create a <Title> react component that renders an <h1> which is
+// centered, palevioletred and sized at 1.5em
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a <Wrapper> react component that renders a <section> with
+// some padding and a papayawhip background
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+// Use them like any other React component – except they're styled!
+
+
+console.log(Title)
 export default function Home() {
 	const style: Style = {
 		container: {
@@ -19,7 +41,7 @@ export default function Home() {
 	return (
 		<div style={style.container}>
 			<Head>
-				<title>hi</title>
+				<title>h1</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
@@ -27,6 +49,11 @@ export default function Home() {
 				<h1 style={style.title}>
 					Welcome to <a href="https://flyingmylo.com">flyingmylo</a>
 				</h1>
+				<div>
+					<Wrapper>
+						<Title>Hello World, this is my first styled component!</Title>
+					</Wrapper>
+				</div>
 			</main>
 
 			<footer></footer>
--- pages/tag/index.tsx
+++ pages/tag/index.tsx
@@ -1,4 +1,4 @@
-
+import React from "react"
 const tag = () => {
   return (
     <div>标签页</div>
